087c3b60cde2144c1f31c8af69c0b05fd2aed005
vitebo <vitebo@hotmail.com>
2020-10-21 23:44:19 -0300

    :sparkles: (build) add webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  devtool: 'inline-source-map',
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].min.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{ loader: 'babel-loader' }]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
+  ]
+};
